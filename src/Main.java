97350cb772ea21bcfb1b241eed178937c25dccd5
Bruno Pavan Adario Moreira <bpmoreira@TOPAZ.CORP>
2024-10-09 11:32:55 -0300

    feat(lab-padroes-projeto-java): add singleton/strategy/facade design patterns examples

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
-import one.digitalinnovation.gof.SingletonEager;
-import one.digitalinnovation.gof.SingletonLazy;
-import one.digitalinnovation.gof.SingletonLazyHolder;
+import one.digitalinnovation.gof.facade.Facade;
+import one.digitalinnovation.gof.singleton.SingletonEager;
+import one.digitalinnovation.gof.singleton.SingletonLazy;
+import one.digitalinnovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinnovation.gof.strategy.*;
 
 public class Main {
     public static void main(String[] args) {
+
+        // Design Pattern Singleton
         SingletonLazy lazy = SingletonLazy.getInstancia();
         System.out.println("Singleton Lazy without \"instancia\": " + lazy);
         lazy = SingletonLazy.getInstancia();
@@ -18,5 +22,25 @@ public class Main {
         System.out.println("Singleton Lazy Holder without \"instancia\": " + lazyHolder);
         lazyHolder = SingletonLazyHolder.getInstancia();
         System.out.println("Singleton Lazy Holder with \"instancia\": " + lazyHolder);
+
+        // Design Pattern Strategy
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento agressivo = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+        robo.setComportamento(normal);
+        robo.mover();
+        robo.mover();
+        robo.setComportamento(defensivo);
+        robo.mover();
+        robo.setComportamento(agressivo);
+        robo.mover();
+        robo.mover();
+        robo.mover();
+
+        // Design Pattern Facade
+        Facade facade = new Facade();
+        facade.migrarCliente("Bruno", "13320070");
     }
 }
